ee504e2f6d951370d94d60df1e62c4e8425c0dac
SencereRabel <Sencererabel@gmail.com>
2024-10-23 17:59:10 -0400

    Added question_bank.py
    It has multiple functions for loading and storing questions to a .txt file(Needs review)

---
+++ question_bank.py
@@ -0,0 +1,50 @@
+
+def load_bank_from_file(num_question):
+    '''
+    When given a number of questions loads that many questions dictonary returning it 
+    '''
+    dict = {}
+    file = open('/Users/senrabel/Documents/Classes/Python Programming/Final Project/Question bank/bank.txt','r')
+    for i in range (num_question):
+        question_raw = file.readline().split(':')
+        q = question_raw[0]
+        ans = question_raw[1]
+        a = ans.split(',')
+        a.remove('\n')
+        dict.update({f'{q}':a})
+    file.close()
+    return dict
+
+def write_bank_to_file(dict):
+    '''
+    Takes the dictonary and writes it into the bank.txt file overwriting anything else in the file
+    '''
+    file = open('/Users/senrabel/Documents/Classes/Python Programming/Final Project/Question bank/bank.txt', 'w')
+    for key, val in dict.items():
+        s = ""
+        for i in val:
+            s += i + ","
+        file.write(f'{key}:{s}\n')
+def add_to_bank(dict):
+    '''
+    Adds new items into the dictonary that stores the question bank DOES NOT write 
+    the bank to a file
+    '''
+    num = int(input("Enter number of questions to add to the bank:"))
+    for i in range(num):
+        q = input("Enter question:")
+        a = []
+        a.append(input("Enter answer:"))
+        for j in range(3):
+            a.append(input("Enter decoy answer:"))
+        dict.update({f'{q}':a})
+    return dict
+
+def print_bank(dict):
+    '''
+    prints the key and values froma given dictonary 
+    '''
+    for key, val in dict.items():
+        print(f'{key}:{val}')
+
+
